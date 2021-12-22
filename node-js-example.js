55555028327e6d700662f424c4b333d1656b110a
haider.sajjad <haider.sajjad@veeve.io>
2021-12-22 11:52:15 +0500

    nodejs file added

---
+++ node-js-example.js
@@ -0,0 +1,9 @@
+var http = require('http');
+ 
+// create a server listening on 8087
+http.createServer(function (req, res) {
+    // write the response and send it to the client
+    res.writeHead(200, {'Content-Type': 'text/html'}); 
+    res.write('Node.js says hello!');
+    res.end();
+}).listen(9051);
